8b77f65b9d7bb0daaf7d013869718005259d4fea
Barath Raj <130913642+Barathaj@users.noreply.github.com>
2024-11-07 00:36:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pdfplumber
-import pymupdf as fitz
+import fitz
 import pandas as pd
 import io
 from PIL import Image
@@ -23,14 +23,35 @@ def extract_text_from_pdf(pdf_path):
             text += page.extract_text()
     return text
 
-def extract_images_from_pdf(pdf_path, output_folder):
-    images = []
+def extract_images_from_pdf(pdf_path):
     doc = fitz.open(stream=pdf_path.read(), filetype="pdf")
-    for page_num in range(len(doc)):
-        page = doc.load_page(page_num)
-        images += page.get_images(full=True)
-    st.write(images)
-    return images
+    image_list_container = []
+    for page_index in range(len(pdf_file)):
+        # get the page itself
+        page = pdf_file[page_index]
+        image_list = page.get_images()
+        # printing number of images found in this page
+        if image_list:
+            if verbose:
+                print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
+        else:
+            if verbose:
+                print("[!] No images found on page", page_index)
+        for image_index, img in enumerate(page.get_images(), start=1):
+            # get the XREF of the image
+            xref = img[0]
+            # extract the image bytes
+            base_image = pdf_file.extract_image(xref)
+            image_bytes = base_image["image"]
+            # get the image extension
+            image_ext = base_image["ext"]
+            # load it to PIL
+            image = Image.open(io.BytesIO(image_bytes))
+            filename = f"image_{page_index+1}_{image_index}.{image_ext}"
+            image_list_container.append((image_bytes, image, filename))
+
+    pdf_file.close()
+    return image_list_container
 
 
 # Streamlit App layout
